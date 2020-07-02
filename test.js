a827fd1643cfa93ac4c6d1224a379cca21966fac
Samuel Greenwald <gozwald@gmail.com>
2020-07-02 12:58:28 +0200

    deleted bootrap

--- test.js
+++
@@ -1,7 +0,0 @@
-var xPathToCss = require("xpath-to-css");
-
-const xPath =
-  '//*[(@id = "anonCarousel6")]//*[contains(concat( " ", @class, " " ), concat( " ", "a-row", " " )) and contains(concat( " ", @class, " " ), concat( " ", "a-size-small", " " ))] | //*[(@id = "anonCarousel5")]//*[contains(concat( " ", @class, " " ), concat( " ", "a-size-small", " " ))] | //*[contains(concat( " ", @class, " " ), concat( " ", "sg-col-12-of-28", " " ))]//*[contains(concat( " ", @class, " " ), concat( " ", "a-size-small", " " ))]';
-const css = xPathToCss(xPath);
-
-console.log(css);
